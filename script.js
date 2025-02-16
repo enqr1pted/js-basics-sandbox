4db35272f37a530235efe82a48a331180994fd43
Andrew <enqr1pted@xmail.ru>
2025-02-16 17:04:05 +0300

    index & script added

---
+++ script.js
@@ -0,0 +1,6 @@
+let userName;
+userName = prompt("What's your name?");
+alert(`Welcome, ${userName}!`);
+
+let testConfirm = confirm("English or Spanish?");
+alert(`Вы выбрали ответ - ${testConfirm}!`);
