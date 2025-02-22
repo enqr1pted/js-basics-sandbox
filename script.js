b1911f59c352d74dc01a53baabf3d8cc95300f0a
Andrew <enqr1pted@xmail.ru>
2025-02-22 22:15:51 +0300

    prime numbers

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let num1 = 10,
-	num2 = 20,
-	result;
+inBegin:
+do { 
+	let n = prompt('Привет. Введи любое число n, и перед тобой появится список простых чисел, в интервале от 2 до n. ');
+let result = `Список простых чисел в интервале от 2 до ${n}: `;
 
-	result ??= num1 ?? num2;
+outer:
+for (let i = 2; i <= n; i++) {
+	for (let j = 2; j < i; j++) {
+		if (i % j == 0) continue outer;
+	}
+	result = result + i + ', ';
+}
+if (n == null || n == undefined) {
+	alert('Не хочешь - как хочешь. Покааа -.-');
+} else if (n == ''){
+	alert('Не оставляй поле пустым, введи число!');
+	continue inBegin;
+} else {
 	alert(result);
+}
+break;
+} while (true)
